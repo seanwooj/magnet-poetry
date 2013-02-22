b0a9912917b73e8824de781bd99568158b701f61
seanwooj <seanwoojinkim@gmail.com>
2013-02-22 15:19:37 -0800

    Initial commit, added bcrypyt, compass, foundation, capybara, pg, and rspec

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'sqlite3'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Frontend Gems
+gem 'compass-rails'
+gem 'zurb-foundation'
+
+group :development, :test do
+  gem 'rspec-rails', '2.11.0'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
+
+gem 'jquery-rails'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagnetPoetry::Application.config.secret_token = '874c1147510aea1ee4c2a748afb7f740e7aba1d8f86f68c59e4e11f2af9801650de9783dbf1662196f86336016f5112d429cccfade6c8fc04374782633cf0980'
