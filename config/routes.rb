120a6633647050e567d084739c7eba29ddc95f4f
seanwooj <seanwoojinkim@gmail.com>
2013-02-25 12:17:31 -0800

    fixing root to

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MagnetPoetry::Application.routes.draw do
 
   resources :users
 
-  match '/', to: 'static_pages#home'
+  root to: 'static_pages#home'
   match 'signup', to: 'users#new'
 
   get "static_pages/about"
